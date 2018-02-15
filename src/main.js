9c3d29c3b60e04e5c4dd6f79adc00536ac3a1b30
kyjoya <kyleejacker@gmail.com>
2018-02-15 15:03:15 -0500

    config updates webpack 2

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './main.scss';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>"Hello World"</h1>,
+  document.getElementById('app')
+);
